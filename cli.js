3f1bd5117d61e5e878359eee600bd3d197b548d5
ArcheCraft <60224415+ArcheCraft@users.noreply.github.com>
2022-02-18 11:57:23 +0100

    Add more commands and file loading

--- cli.js
+++ cli.js
@@ -5,9 +5,9 @@ const prompt = require("prompt");
 const commandQuery = [
   {
     name: "command",
-    validator: /^(w(est)?|s(outh)?|n(orth)?|e(ast)?|exit|help)$/,
+    validator: /^(w(est)?|s(outh)?|n(orth)?|e(ast)?|exit|help|load [^\s]*)$/,
     warning:
-      "Allowed commands are: 'west', 'south', 'north', 'east', 'exit', 'help'",
+      "Allowed commands are: 'west', 'south', 'north', 'east', 'exit', 'help', 'load'",
   },
 ];
 
@@ -15,16 +15,28 @@ function printHelp() {
   println("----- HELP ------");
   println();
   println("--- CONTROLS ---");
-  println("  west    : move west");
-  println("  south   : move south");
-  println("  east   : move south");
-  println("  north   : move south");
+  println("  west        : move west");
+  println("  south       : move south");
+  println("  east        : move south");
+  println("  north       : move south");
   println();
-  println("  exit   : move south");
-  println("  help   : move south");
+  println("  load <file> : load a maze from a file");
+  println();
+  println("  exit        : move south");
+  println("  help        : move south");
   println();
   println("----- HELP ------");
 }
+
+var commands = (exports.commands = {
+  west: 0,
+  south: 1,
+  east: 2,
+  north: 3,
+  load: 4,
+  exit: 5,
+  help: 6,
+});
 
 exports.start = function (onErr) {
   prompt.start();
@@ -38,7 +50,42 @@ exports.nextCommand = function () {
       return onErr(err);
     }
 
+    var cmd = result.command;
+
     println("Command-line input received:");
-    println("  Command: " + result.command);
+    println("  Command: " + cmd);
+
+    if (cmd == "w" || cmd == "west") {
+      return {
+        command: commands.west,
+      };
+    } else if (cmd == "s" || cmd == "south") {
+      return {
+        command: commands.south,
+      };
+    } else if (cmd == "e" || cmd == "east") {
+      return {
+        command: commands.east,
+      };
+    } else if (cmd == "n" || cmd == "north") {
+      return {
+        command: commands.north,
+      };
+    } else if (cmd == "exit") {
+      return {
+        command: commands.help,
+      };
+      return commands.exit;
+    } else if (cmd == "help") {
+      return {
+        command: commands.help,
+      };
+    } else if (cmd.startsWith("load ")) {
+      var file = cmd.split(" ")[1];
+      return {
+        command: commands.load,
+        file: file,
+      };
+    }
   });
 };
--- maze.js
+++ maze.js
@@ -1,39 +1,40 @@
-const utils = require("./utils.js")
+const utils = require("./utils.js");
+const fs = require("fs");
 
-
-const types = {
+var types = (exports.types = {
   wall: 0,
-  stone: 1
-}
+  stone: 1,
+});
 
-
-exports.types = types
-
-exports.create = function (x, y) {
+var create = (exports.create = function (x, y) {
   array = [];
   for (let i = 0; i < x; i++) {
-    layer = []
+    layer = [];
     for (let j = 0; j < y; j++) {
-      layer.push(types.wall)
+      layer.push(types.wall);
     }
-    array.push(layer)
+    array.push(layer);
   }
-};
+});
 
-
-exports.read = function (data) {
-  var lines = utils.lines(data)
-  var result = []
+var read = (exports.read = function (data) {
+  var lines = utils.lines(data);
+  var result = [];
   for (line in lines) {
-    var chars = line.split(" ")
-    var layer = []
+    var chars = line.split(" ");
+    var layer = [];
     for (c in chars) {
-      layer.push(parseInt(c))
+      layer.push(parseInt(c));
     }
-    result.push(layer)
+    result.push(layer);
   }
-}
+});
 
-exports.load = function (path) {
-  // TODO: load file as maze
-}
+var load = (exports.load = function (path, callback) {
+  fs.readFile(path, "utf-8", function (err, data) {
+    if (err) throw err;
+
+    var maze = read(data);
+    callback(maze);
+  });
+});
