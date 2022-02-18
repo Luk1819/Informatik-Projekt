c81b2484493e5bb8ee2a244042e06ad1f85b252e
Lukas <54540778+Luk1819@users.noreply.github.com>
2022-02-18 13:52:31 +0100

    add world.js

---
+++ world.js
@@ -0,0 +1,21 @@
+export const directions = {
+    west: 0,
+    south: 1,
+    east: 2,
+    north: 3,
+}
+
+function walk(dir){
+    if(dir == directions.west){
+
+    }
+    else if(dir == directions.south){
+
+    }
+    else if(dir == directions.east){
+
+    }
+    else if(dir == directions.north){
+
+    }
+}
