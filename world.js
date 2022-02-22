83f25e3e582425204ca3173f3a28930472aeeb3e
ArcheCraft <60224415+ArcheCraft@users.noreply.github.com>
2022-02-22 09:06:39 +0100

    Add world class

--- world.js
+++ world.js
@@ -1,21 +1,45 @@
 export const directions = {
-    west: 0,
-    south: 1,
-    east: 2,
-    north: 3,
-}
+  west: 0,
+  south: 1,
+  east: 2,
+  north: 3,
+};
 
-function walk(dir){
-    if(dir == directions.west){
+class World {
+  maze;
+  entities;
 
+  constructor(maze) {
+    this.maze = maze;
+    this.entities = {
+      0: {}
     }
-    else if(dir == directions.south){
+  }
 
+  get(x, y) {
+    var line = entities[x];
+    if (line) {
+      return line[y];
+    } else {
+      return null;
     }
-    else if(dir == directions.east){
+  }
 
+  set(x, y, value) {
+    var line = entities[x];
+    if (line) {
+      line[y] = value;
+    } else {
+      entities[x] = {};
+      entities[x][y] = value;
     }
-    else if(dir == directions.north){
+  }
 
+  walk(dir) {
+    if (dir == directions.west) {
+    } else if (dir == directions.south) {
+    } else if (dir == directions.east) {
+    } else if (dir == directions.north) {
     }
+  }
 }
