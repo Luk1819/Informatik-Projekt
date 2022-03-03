23d1b6c8805ab639619cb2dba145d20e7650bf5f
Noah Dörr <archecraft1@gmail.com>
2022-03-03 21:30:45 +0100

    Enemies move and attack now

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { println } from "./utils.js";
 import * as maze from "./maze.js";
 import * as world from "./world.js";
 import * as enemies from "./enemies.js";
+import colors from "@colors/colors/safe.js";
 
 cli.start();
 
@@ -35,10 +36,10 @@ const res = await cli.menu(function (cmd) {
     };
 });
 
-console.log(res);
-
 if (res.start) {
     await cli.ingame(currWorld, function (cmd) {
+        let moved = false;
+        
         if (cmd.command == cli.igcommands.exit) {
             println("Exiting!");
             return {
@@ -47,18 +48,26 @@ if (res.start) {
         } else if (cmd.command == cli.igcommands.up) {
             if (!currWorld.walk(world.directions.north)) {
                 println("Illegal move!");
+            } else {
+                moved = true;
             }
         } else if (cmd.command == cli.igcommands.left) {
             if (!currWorld.walk(world.directions.west)) {
                 println("Illegal move!");
+            } else {
+                moved = true;
             }
         } else if (cmd.command == cli.igcommands.down) {
             if (!currWorld.walk(world.directions.south)) {
                 println("Illegal move!");
+            } else {
+                moved = true;
             }
         } else if (cmd.command == cli.igcommands.right) {
             if (!currWorld.walk(world.directions.east)) {
                 println("Illegal move!");
+            } else {
+                moved = true;
             }
         }
         
@@ -72,6 +81,36 @@ if (res.start) {
         
         return {
             cont: true,
+            didMove: moved
+        };
+    }, function () {
+        currWorld.enemyMove();
+        
+        if (currWorld.isFinished()) {
+            return {
+                cont: false,
+                print: true
+            };
+        }
+        
+        return {
+            cont: true
         };
     });
+}
+
+function printSep(size) {
+    println();
+    println(colors.gray("-".repeat(size)));
+    println();
+}
+
+if (!currWorld.survived()) {
+    printSep(14)
+    println(colors.red(colors.italic("YOU'RE DEAD...")));
+    printSep(14)
+} else {
+    printSep(8)
+    println(colors.green(colors.bold("YOU WON!")));
+    printSep(8)
 }
