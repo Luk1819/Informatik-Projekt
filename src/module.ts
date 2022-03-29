5efeb8a3ae3d4dc3818771dd3d3b5127b70ab6d4
ArcheCraft <60224415+ArcheCraft@users.noreply.github.com>
2022-03-29 07:48:23 +0200

    Fix startpos and goal pos

--- src/module.ts
+++ src/module.ts
@@ -39,7 +39,7 @@ export function read(id: string, data: string) {
     if (size[0] != json.maze.length || size[1] != json.maze[0].length) {
         throw Error("Illegal module size, got: " + json.maze.length + ", " + json.maze[0].length + " expected: " + size);
     }
-    return create(id, json.directions, json.maze, json.enemies, json.tiles || {}, json.goalPos || [Math.floor((size[0] + 1) / 2), Math.floor((size[1] + 1) / 2)]);
+    return create(id, json.directions, json.maze, json.enemies, json.tiles || {}, json.goalPos || [Math.floor((size[0] - 1) / 2), Math.floor((size[1] - 1) / 2)]);
 }
 
 export function discover() {
